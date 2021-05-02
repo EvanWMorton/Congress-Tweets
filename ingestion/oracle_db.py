de8f6f734af0aa581ce4c79af69fdd7d44bf31fd
Evan W. Morton <evanwmorton@gmail.com>
2021-05-02 11:18:31 -0400

    Cleanup, some of which is for pylint.

--- ingestion/oracle_db.py
+++ ingestion/oracle_db.py
@@ -1,9 +1,13 @@
+'''
+This class attempts to isolate the Oracle-specific aspects of
+database access.  If we switch to another database, the first step
+would be to create an abstract class and make this a child of that.
+'''
 import cx_Oracle
 import read_config
 
-# This class attempts to isolate the Oracle-specific aspects of database access.  If we switch to another
-# database, the first step would be to create an abstract class and make this a child of that.
 class OracleDb:
+    ''' See module comment '''
     # This is a class variable.
     have_initialized_oracle_client_lib = False
 
@@ -22,37 +26,41 @@ class OracleDb:
         database = db_config["database"]
         connstr = username + "/" + password + "@" + endpoint + ":" + port + "/" + database
         self.connection = cx_Oracle.connect(connstr)
-        
-    def get_some_stuff(self):
-        cursor = self.connection.cursor()
-        #Execute Query
-        cursor.execute("select * from raw_tweet")
-        result = cursor.fetchall()
-        #Fetch results
-        for row in result:
-            print(row)
-        cursor.close()
+
+#    def get_some_stuff(self):
+#        cursor = self.connection.cursor()
+#        #Execute Query
+#        cursor.execute("select * from raw_tweet")
+#        result = cursor.fetchall()
+#        #Fetch results
+#        for row in result:
+#            print(row)
+#        cursor.close()
     def run_query(self, query):
+        ''' Run a database query and return the result '''
         cursor = self.connection.cursor()
         cursor.execute(query)
         result = cursor.fetchall()
         cursor.close()
         return result
     def run_statement(self, statement):
+        ''' Run a database statement '''
         cursor = self.connection.cursor()
         cursor.execute(statement)
         # How do you resturn result?
-    # Use bind variables.
-    def run_statement_with_variables(self, statement, dict):
-        #print ("count of variables: " + str(len(dict)))
+    def run_statement_with_variables(self, statement, dictionary):
+        ''' Run a database statement with bind variables. '''
         cursor = self.connection.cursor()
-        cursor.execute(statement, dict)
+        cursor.execute(statement, dictionary)
         # How do you resturn result?
     def commit(self):
+        ''' Perform a COMMIT. '''
         self.connection.commit()
     def close(self):
+        ''' Close the database connection. '''
         self.connection.close()
     def get_date_format_string():
+        ''' Get a date format which, in the TO_DATE function for this database, gives a format that works for INSERT statements. '''
         return 'YYYY-MM-DD-HH24-MI-SS'
 
 #odb = OracleDb()
--- ingestion/read_config.py
+++ ingestion/read_config.py
@@ -1,16 +1,18 @@
+''' This module is to read a JSON config file and returns a dictionary with its contents.
+In the case of a config file with auth info, the keys are based on what
+the Twitter developer site calls the variables when you receive them, not on what Tweepy
+(and presumably Twitter) calls them in doc on authorization.  So they include:
+    api_key
+    api_secret_key
+    bearer_token
+    access_token
+    access_token_secret
+'''
+
 import json
 
-# Reads a JSON config file and returns a dictionary with its contents.
-# 
-# In the case of a config file with auth info, the keys are based on what
-# the Twitter developer site calls the variables when you receive them, not on what Tweepy
-# (and presumably Twitter) calls them in doc on authorization.  So they are:
-#     api_key
-#     api_secret_key
-#     bearer_token
-#     access_token
-#     access_token_secret
 def read_config(filename):
+    # pylint: disable=missing-function-docstring
     with open(filename, "r") as jsonfile:
         config_data = json.load(jsonfile)
         #print("config read successful")
